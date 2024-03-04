a38db137d5ef88b5332b67c4543fb99b5ccb17fe
Barashuk1 <130751763+Barashuk1@users.noreply.github.com>
2024-03-04 20:26:32 +0200

    Update query_7.sql

--- homework6/query/query_7.sql
+++ homework6/query/query_7.sql
@@ -3,4 +3,4 @@ FROM Students
 JOIN Data ON Students.id = Data.id_student
 JOIN Marks ON Data.id_mark = Marks.id
 JOIN Subjects ON Data.id_subject = Subjects.id
-WHERE Students.id_group = 4 AND Subjects.name = 'some';
+WHERE Students.id_group = 4 AND Subjects.name = 'some'; -- change
