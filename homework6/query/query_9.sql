d1654dc2df3025eca9d585beecde6b86109de25a
Barashuk1 <130751763+Barashuk1@users.noreply.github.com>
2024-03-04 20:26:50 +0200

    Update query_9.sql

--- homework6/query/query_9.sql
+++ homework6/query/query_9.sql
@@ -2,4 +2,4 @@ SELECT Subjects.name
 FROM Subjects
 JOIN Data ON Subjects.id = Data.id_subject
 JOIN Students ON Data.id_student = Students.id
-WHERE Students.id = 4;
+WHERE Students.id = 4; -- change
